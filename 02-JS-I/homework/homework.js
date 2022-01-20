f5c883e3f3ae169c05f1a4f5c9e1816b842a8f36
Henry E. Herrera <heslava0520@gmail.com>
2022-01-19 22:09:56 -0500

    Ãfisrt commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = "hola guas";
 const nuevoNum = null;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
